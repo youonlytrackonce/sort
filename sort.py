283ddc6d17ddb0adf2e5af3b35445755b7d8ef8d
youonlytrackonce <f_emre87@hotmail.com>
2022-06-14 18:23:44 +0300

    show track id added

--- sort.py
+++ sort.py
@@ -279,7 +279,7 @@ if __name__ == '__main__':
   total_frames = 0
   colours = np.random.rand(32, 3) #used only for display
   if(display):
-    if not os.path.exists('mot_benchmark'):
+    if not os.path.exists(args.seq_path):
       print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    ('
             'https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s '
             '/path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
@@ -308,13 +308,13 @@ if __name__ == '__main__':
         dets = seq_dets[seq_dets[:, 0]==frame, 2:7]
         dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
         det_num = len(dets[:, 0])
-        eras_num = int((det_num/4))
+        eras_num = int((det_num/10))
         ra_in = random.sample(range(0, det_num), eras_num)
         dets_ = np.delete(dets, ra_in, 0)
         total_frames += 1
 
         if(display):
-          fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
+          fn = os.path.join(args.seq_path, phase, seq, 'img1', '%06d.jpg'%(frame))
           im =io.imread(fn)
           ax1.imshow(im)
           plt.title(seq + ' Tracked Targets')
@@ -329,6 +329,7 @@ if __name__ == '__main__':
           if(display):
             d = d.astype(np.int32)
             ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
+            ax1.text(d[0],d[1],d[4], color=colours[d[4]%32,:])
 
         if(display):
           fig.canvas.flush_events()
